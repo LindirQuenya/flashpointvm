2826356fe6a1b7636b6964fdcda6a100798d64a2
Joel Puig Rubio <joel.puig.rubio@gmail.com>
2021-09-16 17:27:19 -0400

    Add script to take snapshot when VM is ready

---
+++ snapshot.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+qemu-system-i386 $QEMU_EXTRA_ARGS -display none -m 512 \
+-net 'nic,model=e1000' -net 'user,hostfwd=tcp::22500-:80' \
+-monitor 'tcp:localhost:4445,server,nowait' -hda 'alpine.qcow2' &
+pid=$!
+
+while ! curl -sI 'http://127.0.0.1:22500/' >/dev/null; do
+	echo 'VM not ready .. sleeping for 5 seconds' && sleep 5
+done
+
+echo 'apache is ready .. snapshot in 10 seconds' && sleep 10
+echo 'savevm quick' | nc -q1 -w5 127.0.0.1 4445 >/dev/null
+kill -TERM "$pid"
